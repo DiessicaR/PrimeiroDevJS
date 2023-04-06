4c401b617669e6c3311907463551d9dbf26a0a37
Diessica <diessicarrc@gmail.com>
2023-04-05 22:08:43 -0300

    first commit

---
+++ PrimeiroDevJS/script.js
@@ -0,0 +1,29 @@
+// meu primeiro comentário 
+
+/* meu segundo comentário */
+
+console.log('Olá galera de caubói')
+
+prompt("Quem gosta de rodeio?")
+
+//conhecendo e trabalhando com variáveis
+
+let texto = "Meu dia de nascimento é: "
+const diaDeNascimento = 09;
+
+console.log(texto);
+console.log(diaDeNascimento);
+
+//concatenando variáveis
+
+console.log(texto + diaDeNascimento);
+
+
+//exercicios de fixação
+
+const primeiroNome = "Diéssica";
+const sobrenome = "da Cruz";
+let idade = 30; 
+let souEstudante = true;
+
+console.log( "Meu nome é " + primeiroNome, sobrenome, "Minha idade: " + idade , "Estou estudando? " + souEstudante);
